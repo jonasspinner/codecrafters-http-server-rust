bfd49e73d8e03d06da4e21346497ad1fa7ececb3
Jonas Spinner <jonas.spinner@student.kit.edu>
2024-07-28 11:27:03 +0200

    pass stage ih0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
-use nom::AsBytes;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -9,7 +8,15 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).expect("200");
+                let buf_reader = BufReader::new(&mut stream);
+
+                let request_line = buf_reader.lines().next().unwrap().unwrap();
+                let response = match request_line.as_str() {
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+
+                stream.write_all(response.as_bytes()).unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
