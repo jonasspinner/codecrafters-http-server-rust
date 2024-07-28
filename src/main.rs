6d77d8c453abcf4bfd40699833857e1b51f4fdb2
Jonas Spinner <jonas.spinner@student.kit.edu>
2024-07-28 10:34:51 +0200

    pass stage ia4

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::io::Write;
 use std::net::TcpListener;
+use nom::AsBytes;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).expect("200");
             }
             Err(e) => {
                 println!("error: {}", e);
